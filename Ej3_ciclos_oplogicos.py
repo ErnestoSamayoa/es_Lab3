e6015aa53b7603b54ca265792d9a53a2a805147c
ErnestoSamayoa <esamayoaj1@miumg.edu.gt>
2024-02-01 18:42:54 -0600

    Ejercicio#3_Ciclos

---
+++ Ej3_ciclos_oplogicos.py
@@ -0,0 +1,10 @@
+#Desarrolla un programa que imprima los primeros 10 números pares utilizando un ciclo while o for.
+
+print("*************Bienvenido Usuario************\n")
+
+print("Los primeros 10 números pares son:\n ")
+for i in range(2, 21, 2):  
+ print(i,end=" , ")
+
+print("\n")
+print("*********************************************")
